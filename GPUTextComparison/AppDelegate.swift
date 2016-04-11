504bfcc9f0d780b36576f08871bff604de4bb475
litherum <litherum@gmail.com>
2016-04-10 19:26:38 -0700

    Initial Commit

---
+++ GPUTextComparison/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GPUTextComparison
+//
+//  Created by Litherum on 4/10/16.
+//  Copyright © 2016 Litherum. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
