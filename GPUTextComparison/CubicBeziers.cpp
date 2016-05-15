ccc42ac518eac6f4deb67a9c6e8b51864f630e68
litherum <litherum@gmail.com>
2016-05-15 16:49:49 -0700

    Loops seem to work okay

--- GPUTextComparison/CubicBeziers.cpp
+++ GPUTextComparison/CubicBeziers.cpp
@@ -39,6 +39,11 @@ struct Coefficients {
     bool flip;
 };
 
+struct Subdivision {
+    std::array<CGPoint, 4> points;
+    Coefficients c;
+};
+
 static Coefficients lineOrPoint(CGFloat d1, CGFloat d2, CGFloat d3) {
     return {
         { 0, 0, 0 },
@@ -73,64 +78,14 @@ static Coefficients serpentine(CGFloat d1, CGFloat d2, CGFloat d3) {
     };
 }
 
-static Coefficients loop(CGFloat d1, CGFloat d2, CGFloat d3) {
-    CGFloat ls = d2 - std::sqrt(4 * d1 * d3 - 3 * d2 * d2);
-    CGFloat lt = 2 * d1;
-    CGFloat ms = d2 + std::sqrt(4 * d1 * d3 - 3 * d2 * d2);
-    CGFloat mt = 2 * d1;
-    return {
-        { ls * ms, ls * ls * ms, ls * ms * ms },
-        { (-ls * mt - lt * ms + 3 * ls * ms) / 3, ls * (ls * (mt - 3 * ms) + 2 * lt * ms) / -3, ms * (ls * (2 * mt - 3 * ms) + lt * ms) / -3 },
-        { (lt * (mt - 2 * ms) + ls * (3 * ms - 2 * mt)) / 3, (lt - ls) * (ls * (2 * mt - 3 * ms) + lt * ms) / 3, (mt - ms) * (ls * (mt - 3 * ms) + 2 * lt * ms) / 3 },
-        { (lt - ls) * (mt - ms), -(lt - ls) * (lt - ls) * (mt - ms), -(lt - ls) * (mt - ms) * (mt - ms) },
-        true
-    }; // FIXME: might need to subdivide and update orientation
-}
-
-static Coefficients cusp(CGFloat d1, CGFloat d2, CGFloat d3) {
-    CGFloat ls = d3;
-    CGFloat lt = 3 * d2;
-    return {
-        { ls, ls * ls * ls, 1 },
-        { ls - lt / 3, ls * ls * (ls - lt), 1 },
-        { ls - 2 * lt / 3, (ls - lt) * (ls - lt) * ls, 1 },
-        { ls - lt, (ls - lt) * (ls - lt) * (ls - lt), 1 },
-        true
-    };
-}
-
 static inline CGFloat roundToZero(CGFloat x) {
-    const CGFloat epsilon = 0.001;
+    const CGFloat epsilon = 0.0001;
     if (std::abs(x) < epsilon)
         return 0;
     return x;
 }
 
-static inline CubicVertex convertTriangulatedVertex(Triangulation::Vertex& v) {
-    auto& point = v.point();
-    auto& info = v.info();
-    return { CGPointMake(point.x(), point.y()),
-        { static_cast<float>(info.k), static_cast<float>(info.l), static_cast<float>(info.m) } };
-}
-
-static inline std::vector<std::array<CubicVertex, 3>> triangulate(CGPoint p0, std::array<CGFloat, 3> c0, CGPoint p1, std::array<CGFloat, 3> c1, CGPoint p2, std::array<CGFloat, 3> c2, CGPoint p3, std::array<CGFloat, 3> c3) {
-    Triangulation t;
-    auto v0 = t.insert(Triangulation::Point(p0.x, p0.y));
-    v0->info() = { c0[0], c0[1], c0[2] };
-    auto v1 = t.insert(Triangulation::Point(p1.x, p1.y));
-    v1->info() = { c1[0], c1[1], c1[2] };
-    auto v2 = t.insert(Triangulation::Point(p2.x, p2.y));
-    v2->info() = { c2[0], c2[1], c2[2] };
-    auto v3 = t.insert(Triangulation::Point(p3.x, p3.y));
-    v3->info() = { c3[0], c3[1], c3[2] };
-
-    std::vector<std::array<CubicVertex, 3>> result;
-    for (auto i = t.finite_faces_begin(); i != t.finite_faces_end(); ++i)
-        result.push_back({ convertTriangulatedVertex(*i->vertex(0)), convertTriangulatedVertex(*i->vertex(1)), convertTriangulatedVertex(*i->vertex(2)) });
-    return result;
-}
-
-void cubic(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CubicFaceReceiver receiver) {
+static inline std::array<CGFloat, 3> computeDs(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3) {
     CGAL::Vector_3<K> b0(p0.x, p0.y, 1);
     CGAL::Vector_3<K> b1(p1.x, p1.y, 1);
     CGAL::Vector_3<K> b2(p2.x, p2.y, 1);
@@ -152,10 +107,138 @@ void cubic(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CubicFaceReceiver rec
     d2 = roundToZero(d2);
     d3 = roundToZero(d3);
 
+    return { d1, d2, d3 };
+}
+
+static inline CGPoint subdivide(CGFloat t, CGPoint a, CGPoint b) {
+    return CGPointMake((1 - t) * a.x + t * b.x, (1 - t) * a.y + t * b.y);
+}
+
+static inline std::array<std::array<CGPoint, 4>, 2> subdivide(CGFloat t, CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3) {
+    auto ab = subdivide(t, p0, p1);
+    auto bc = subdivide(t, p1, p2);
+    auto cd = subdivide(t, p2, p3);
+    auto abc = subdivide(t, ab, bc);
+    auto bcd = subdivide(t, bc, cd);
+    auto abcd = subdivide(t, abc, bcd);
+
+    return {{ { p0, ab, abc, abcd }, { abcd, bcd, cd, p3 } }};
+}
+
+static inline std::array<CGFloat, 4> loopParameters(CGFloat d1, CGFloat d2, CGFloat d3) {
+    CGFloat ls = d2 - std::sqrt(4 * d1 * d3 - 3 * d2 * d2);
+    CGFloat lt = 2 * d1;
+    CGFloat ms = d2 + std::sqrt(4 * d1 * d3 - 3 * d2 * d2);
+    CGFloat mt = 2 * d1;
+
+    return { ls, lt, ms, mt };
+}
+
+template <typename T>
+static inline T sign(T x) {
+    return x > 0 ? 1 : (x < 0 ? -1 : 0);
+}
+
+static inline Coefficients loopCoefficients(CGFloat d1, CGFloat ls, CGFloat lt, CGFloat ms, CGFloat mt) {
+    Coefficients result = {
+        { ls * ms, ls * ls * ms, ls * ms * ms },
+        { (-ls * mt - lt * ms + 3 * ls * ms) / 3, ls * (ls * (mt - 3 * ms) + 2 * lt * ms) / -3, ms * (ls * (2 * mt - 3 * ms) + lt * ms) / -3 },
+        { (lt * (mt - 2 * ms) + ls * (3 * ms - 2 * mt)) / 3, (lt - ls) * (ls * (2 * mt - 3 * ms) + lt * ms) / 3, (mt - ms) * (ls * (mt - 3 * ms) + 2 * lt * ms) / 3 },
+        { (lt - ls) * (mt - ms), -(lt - ls) * (lt - ls) * (mt - ms), -(lt - ls) * (mt - ms) * (mt - ms) },
+        false
+    };
+    result.flip = (d1 > 0 && sign(result.v1[0]) < 0) || (d1 < 0 && sign(result.v1[0]) > 0);
+    return result;
+}
+
+static std::vector<Subdivision> loop(CGFloat d1, CGFloat d2, CGFloat d3, CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3) {
+    CGFloat ls, lt, ms, mt;
+    std::tie(ls, lt, ms, mt) = loopParameters(d1, d2, d3);
+
+    CGFloat t0 = ms / mt;
+    CGFloat t1 = ls / lt;
+    bool c0 = t0 > 0 && t0 < 1;
+    bool c1 = t1 > 0 && t1 < 1;
+    if (c0 || c1) {
+        // We need to subdivide.
+        auto t = c0 ? t0 : t1;
+        auto subdivided = subdivide(t, p0, p1, p2, p3);
+        std::tie(d1, d2, d3) = computeDs(subdivided[0][0], subdivided[0][1], subdivided[0][2], subdivided[0][3]);
+        std::tie(ls, lt, ms, mt) = loopParameters(d1, d2, d3);
+        auto coefficients1 = loopCoefficients(d1, ls, lt, ms, mt);
+        std::tie(d1, d2, d3) = computeDs(subdivided[1][0], subdivided[1][1], subdivided[1][2], subdivided[1][3]);
+        std::tie(ls, lt, ms, mt) = loopParameters(d1, d2, d3);
+        auto coefficients2 = loopCoefficients(d1, ls, lt, ms, mt);
+
+        Subdivision a = { subdivided[0], coefficients1 };
+        Subdivision b = { subdivided[1], coefficients2 };
+        return { a, b };
+    }
+
+    return { { { p0, p1, p2, p3 }, loopCoefficients(d1, ls, lt, ms, mt) } };
+}
+
+static Coefficients cusp(CGFloat d1, CGFloat d2, CGFloat d3) {
+    CGFloat ls = d3;
+    CGFloat lt = 3 * d2;
+    return {
+        { ls, ls * ls * ls, 1 },
+        { ls - lt / 3, ls * ls * (ls - lt), 1 },
+        { ls - 2 * lt / 3, (ls - lt) * (ls - lt) * ls, 1 },
+        { ls - lt, (ls - lt) * (ls - lt) * (ls - lt), 1 },
+        true
+    };
+}
+
+static inline CubicVertex convertTriangulatedVertex(Triangulation::Vertex& v) {
+    auto& point = v.point();
+    auto& info = v.info();
+    return { CGPointMake(point.x(), point.y()),
+        { static_cast<float>(info.k), static_cast<float>(info.l), static_cast<float>(info.m) } };
+}
+
+static inline std::vector<std::array<CubicVertex, 3>> triangulate(Subdivision s) {
+    assert(!s.c.flip);
+
+    Triangulation t;
+    auto v0 = t.insert(Triangulation::Point(s.points[0].x, s.points[0].y));
+    v0->info() = { s.c.v0[0], s.c.v0[1], s.c.v0[2] };
+    auto v1 = t.insert(Triangulation::Point(s.points[1].x, s.points[1].y));
+    v1->info() = { s.c.v1[0], s.c.v1[1], s.c.v1[2] };
+    auto v2 = t.insert(Triangulation::Point(s.points[2].x, s.points[2].y));
+    v2->info() = { s.c.v2[0], s.c.v2[1], s.c.v2[2] };
+    auto v3 = t.insert(Triangulation::Point(s.points[3].x, s.points[3].y));
+    v3->info() = { s.c.v3[0], s.c.v3[1], s.c.v3[2] };
+
+    std::vector<std::array<CubicVertex, 3>> result;
+    for (auto i = t.finite_faces_begin(); i != t.finite_faces_end(); ++i)
+        result.push_back({ convertTriangulatedVertex(*i->vertex(0)), convertTriangulatedVertex(*i->vertex(1)), convertTriangulatedVertex(*i->vertex(2)) });
+    return result;
+}
+
+static void flipCoefficients(Coefficients& coefficients) {
+    if (!coefficients.flip)
+        return;
+
+    coefficients.v0[0] *= -1;
+    coefficients.v0[1] *= -1;
+    coefficients.v1[0] *= -1;
+    coefficients.v1[1] *= -1;
+    coefficients.v2[0] *= -1;
+    coefficients.v2[1] *= -1;
+    coefficients.v3[0] *= -1;
+    coefficients.v3[1] *= -1;
+    coefficients.flip = false;
+}
+
+void cubic(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CubicFaceReceiver receiver) {
+    CGFloat d1, d2, d3;
+    std::tie(d1, d2, d3) = computeDs(p0, p1, p2, p3);
+
     Coefficients result;
     CGFloat discr = d1 * d1 * (3 * d2 * d2 - 4 * d1 * d3);
 
-    if (b0 == b1 && b0 == b2 && b0 == b3)
+    if (CGPointEqualToPoint(p0, p1) && CGPointEqualToPoint(p0, p2) && CGPointEqualToPoint(p0, p3))
         result = lineOrPoint(d1, d2, d3);
     else if (d1 == 0 && d2 == 0 && d3 == 0)
         result = lineOrPoint(d1, d2, d3);
@@ -163,23 +246,19 @@ void cubic(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CubicFaceReceiver rec
         result = quadratic(d1, d2, d3);
     else if (discr > 0)
         result = serpentine(d1, d2, d3);
-    else if (discr < 0)
-        result = loop(d1, d2, d3);
-    else
+    else if (discr < 0) {
+        for (auto subdivision : loop(d1, d2, d3, p0, p1, p2, p3)) {
+            flipCoefficients(subdivision.c);
+            for (auto triangle : triangulate(subdivision))
+                receiver(triangle[0], triangle[1], triangle[2]);
+        }
+        return;
+    } else
         result = cusp(d1, d2, d3);
 
-    if (result.flip) {
-        result.v0[0] *= -1;
-        result.v0[1] *= -1;
-        result.v1[0] *= -1;
-        result.v1[1] *= -1;
-        result.v2[0] *= -1;
-        result.v2[1] *= -1;
-        result.v3[0] *= -1;
-        result.v3[1] *= -1;
-    }
+    flipCoefficients(result);
 
-    for (auto triangle : triangulate(p0, result.v0, p1, result.v1, p2, result.v2, p3, result.v3))
+    for (auto triangle : triangulate({ { p0, p1, p2, p3 }, result }))
         receiver(triangle[0], triangle[1], triangle[2]);
 
     //CGAL::orientation(CGAL::Point_2<K>(p0.x, p0.y), CGAL::Point_2<K>(p3.x, p3.y), CGAL::Point_2<K>(p1.x, p1.y)) == CGAL::RIGHT_TURN,
